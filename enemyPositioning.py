e32bee42dbe16cd4ac38d199df1d65931580ec3a
Xiwen Li <lix4@rose-hulman.edu>
2018-03-10 21:54:31 -0500

    Found Optimal Exposure

--- enemyPositioning.py
+++ enemyPositioning.py
@@ -3,43 +3,42 @@ import paho.mqtt.client as mqtt
 import numpy as np
 import math
 
-videoIn = cv2.VideoCapture(1)
-videoIn.set(cv2.CAP_PROP_BRIGHTNESS,0.05)
-videoIn.set(cv2.CAP_PROP_EXPOSURE, 10)
-print("capture device is open: " + str(videoIn.isOpened()))
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+# client = mqtt.Client()
+# client.on_connect = on_connect
+# client.on_message = on_message
 
 def process_camera():
-	ret = 1
-	if (ret): 
-		while 1:
-			ret, frame_webcam = videoIn.read()
+    videoIn = cv2.VideoCapture(1)
+    videoIn.set(cv2.CAP_PROP_BRIGHTNESS, 0.05)
+    videoIn.set(cv2.CAP_PROP_EXPOSURE, -9)
+    print("capture device is open: " + str(videoIn.isOpened()))
+    ret = 1
+    if (ret):
+        while 1:
+            ret, frame_webcam = videoIn.read()
 
-			#frame_webcam = cv2.cvtColor(frame_webcam, cv2.COLOR_BGR2RGB);
-			frame = cv2.cvtColor(frame_webcam, cv2.COLOR_RGB2HSV)
+            #frame_webcam = cv2.cvtColor(frame_webcam, cv2.COLOR_BGR2RGB);
+            frame = cv2.cvtColor(frame_webcam, cv2.COLOR_RGB2HSV)
 
-			# define range of color red in HSV space
-			upper_red = np.array([10, 255, 255])
-			lower_red = np.array([0, 100, 100])
+            # define range of color red in HSV space
+            upper_red = np.array([10, 255, 255])
+            lower_red = np.array([0, 100, 100])
 
-			# create color red mask
-			mask = cv2.inRange(frame, lower_red, upper_red)
+            # create color red mask
+            mask = cv2.inRange(frame, lower_red, upper_red)
 
-			res = cv2.bitwise_and(frame, frame, mask= mask)
+            res = cv2.bitwise_and(frame, frame, mask=mask)
 
-			cv2.imshow('orig', frame_webcam)
-			cv2.imshow('frame', frame)
-			cv2.imshow('mask', mask)
-			cv2.imshow('res', res)
-			# kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(25, 25))  
-			# frame = cv2.GaussianBlur(frame, (3, 3), 1.2);
-			cv2.waitKey(1)
-	else:
-		raise RuntimeError("Error while reading from camera.")
-
+            cv2.imshow('orig', frame_webcam)
+            cv2.imshow('frame', frame)
+            cv2.imshow('mask', mask)
+            cv2.imshow('res', res)
+            # kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(25, 25))
+            # frame = cv2.GaussianBlur(frame, (3, 3), 1.2);
+            cv2.waitKey(1)
+    else:
+        raise RuntimeError("Error while reading from camera.")
 
 
 # def on_connect(client, userdata, flags, rc):
@@ -57,5 +56,9 @@ def process_camera():
 #         sock.send(can_pkt)
 #         print(BSP_ERROR.info("SocketCAN Package Send"))
 
-def main(parameter_list):
+def main():
     process_camera()
+
+
+if __name__ == "__main__":
+    main()
