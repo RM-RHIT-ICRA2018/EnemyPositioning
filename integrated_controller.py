82b10a339d5cfbe548e6bff6b53cdceea90aa71c
Xiwen Li <lix4@rose-hulman.edu>
2018-05-03 19:19:53 -0400

    Started integrated controller

---
+++ integrated_controller.py
@@ -0,0 +1,33 @@
+import paho.mqtt.client as mqtt
+import time
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Successfully connected with result code " + str(rc))
+        client.subscribe("/ENEMIES")
+    else:
+        print("Bad connection returned code=", rc)
+
+def on_message(client, userdata, message):
+    print("message received ", str(message.payload.decode("utf-8")))
+    print("message topic=", message.topic)
+    print("message qos=", message.qos)
+    print("message retain flag=", message.retain)
+
+def on_subscribe(client, userdata, mid, granted_qos):
+    print("Successfully subscribe topic" + str(client))
+
+
+def main():
+    client = mqtt.Client("ENEMY_IC")
+    broker_address = "192.168.1.2"
+    client.on_connect = on_connect  # attach function to callback
+    client.on_message = on_message  # attach function to callback
+    client.on_subscribe = on_subscribe
+
+    client.connect(broker_address, 1883, 60)
+    print("MQTT client connecting to host []")
+    client.loop_forever()
+
+if __name__ == "__main__":
+    main()
